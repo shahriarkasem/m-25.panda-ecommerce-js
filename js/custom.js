90e0283c36404e8cac4339d0f35ee63244378ce9
Shahriar Kasem <shahriarkasem@gmail.com>
2022-02-10 08:20:39 +0600

    commit

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,9 @@
+// exploring js
 // first test
 const jsTesting = document.getElementById('btn-body-color');
 jsTesting.onclick = makeTomato;
 function makeTomato(){
-    document.body.style.backgroundColor = 'tomato';
+    document.body.style.backgroundColor = 'lightgreen';
 }
 // second test
 document.getElementById('btn-main-color').addEventListener('click', function(){
@@ -11,3 +12,15 @@ document.getElementById('btn-main-color').addEventListener('click', function(){
 
 // task 2
 
+
+// task 3
+document.getElementById('backpack').style.backgroundColor = 'tomato';
+
+// task 4
+document.getElementsByClassName('card').style.borderRadius = '30px';
+// doesn't working
+
+// task 5
+function clickMeOnceMore(){
+    console.log('lets explore js')
+}
